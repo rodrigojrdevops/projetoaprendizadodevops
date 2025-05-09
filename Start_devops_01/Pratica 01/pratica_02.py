f8bffa7555cda2bff7549147a8f0eff778c36368
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-05-08 21:43:45 -0300

    Create pratica_02.py

---
+++ Start_devops_01/Pratica 01/pratica_02.py
@@ -0,0 +1,12 @@
+#Nessa segunda etapa quero criar um codigo que imprima a soma de dois numeros.
+
+def soma(a, b):
+    return a + b
+
+resultado = soma(2, 3)
+  print(f"O resultado da soma é {resultado}")
+  
+  if resultado < 0 :
+      print("O resultado é negativo")
+      pass
+   
